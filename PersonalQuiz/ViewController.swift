ca426989fb80ca28590632109b5c711cf4423712
Artem Gukov <artem.gukov@gmail.com>
2019-03-14 20:32:03 +0300

    Initial Commit

---
+++ PersonalQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalQuiz
+//
+//  Created by Артем on 14/03/2019.
+//  Copyright © 2019 Gukov.space. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
